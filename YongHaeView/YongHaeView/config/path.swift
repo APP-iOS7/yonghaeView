2384aa8104203e9d60f87498ad4098234cce5690
yohans <rhdygks231@naver.com>
2025-02-18 16:44:32 +0900

    bugfix: bugfix

---
+++ YongHaeView/YongHaeView/config/path.swift
@@ -0,0 +1,12 @@
+//Ver : 0_0_2
+//Date : 2025-02-15
+//Contents : Path Group
+
+import Foundation
+
+//File Path
+let PATH_FOLDERNAME = "InitImage"
+
+
+//URL Path
+let PATH_URL_TESTPOST = "http://localhost:8000/api"
